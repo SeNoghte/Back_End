0953d2d80fa46e61e2aa655658844fcada5da0ad
sadegh zareian <zareian.1381@gmail.com>
2024-10-31 22:37:21 +0330

    set data base

---
+++ DataAccess/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccess;
+
+public class ApplicationDBContext : DbContext
+{
+    protected readonly IConfiguration configuration;
+
+    public ApplicationDBContext(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(configuration.GetConnectionString("JalaletoDB"));
+    }
+}

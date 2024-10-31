20998a995d7523493c0d42e612dd984b7b1e20b6
mahdimp <madanipour66@gmail.com>
2024-10-31 15:27:49 +0330

    google auth setup

--- Application/ServiceExtensions.cs
+++ Application/ServiceExtensions.cs
@@ -1,6 +1,7 @@
-using System.Reflection;
-using System.Runtime.Loader;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Application;
 
@@ -12,5 +13,21 @@ public static class ServiceExtensions
         {
             config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
         });
+
+        services.AddAuthentication(config =>
+        {
+            config.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            config.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            config.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+        })
+        .AddCookie()
+        .AddGoogle(op =>
+        {
+            op.ClientId = "178853996623-7d8dh0tal921q54iju05fhqhqdm03gen.apps.googleusercontent.com";
+            op.ClientSecret = "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";
+        });
+
+        services.AddAuthentication();
+        services.AddAuthorization();
     }
 }
